650c3c91db29710b0a8a7cc0f6aad932f97bf906
Jose Felix Rivas Carrasco <josefelixrc@outlook.com>
2021-03-25 12:47:23 +0000

    feat: Add a class for levels

---
+++ include/level.h
@@ -0,0 +1,26 @@
+/*
+ * level.h
+ * 
+ * Copyright 2021 CPW Online <support@cpwonline.org>
+ * 
+ * This program is free software; see the License file for more details.
+ * 
+ * You should have received a copy of the License along with this program.
+ * 
+ */
+
+
+#ifndef LEVEL_H
+#define LEVEL_H
+
+class Level
+{
+	public:
+		Level();
+		virtual ~Level();
+	
+	private:
+		/* add your private declarations */
+};
+
+#endif /* LEVEL_H */ 
---
+++ src/level.cc
@@ -0,0 +1,26 @@
+/*
+ * level.cc
+ * 
+ * Copyright 2021 CPW Online <support@cpwonline.org>
+ * 
+ * This program is free software; see the License file for more details.
+ * 
+ * You should have received a copy of the License along with this program.
+ * 
+ */
+
+
+#include "level.h"
+
+
+Level::Level()
+{
+	
+}
+
+
+Level::~Level()
+{
+	
+}
+
