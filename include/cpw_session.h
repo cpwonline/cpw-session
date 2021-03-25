7827ad85c8b89046bc25d5b06406c8e650b2ab6d
Jose Felix Rivas Carrasco <josefelixrc@outlook.com>
2021-03-25 12:44:24 +0000

    feat: Add the main class `CPWSession`

---
+++ include/cpw_session.h
@@ -0,0 +1,26 @@
+/*
+ * cpw_session.h
+ * 
+ * Copyright 2021 CPW Online <support@cpwonline.org>
+ * 
+ * This program is free software; see the License file for more details.
+ * 
+ * You should have received a copy of the License along with this program.
+ * 
+ */
+
+
+#ifndef CPW_SESSION_H
+#define CPW_SESSION_H
+
+class CPWSession
+{
+	public:
+		CPWSession();
+		virtual ~CPWSession();
+	
+	private:
+		/* add your private declarations */
+};
+
+#endif /* CPW_SESSION_H */ 
