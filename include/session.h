da9e7a3225c9c77478a64043c8f272a2777675d3
Jose Felix Rivas Carrasco <josefelixrc@outlook.com>
2021-03-25 12:45:06 +0000

    feat: Add a class for handle the sessions

---
+++ include/session.h
@@ -0,0 +1,26 @@
+/*
+ * session.h
+ * 
+ * Copyright 2021 CPW Online <support@cpwonline.org>
+ * 
+ * This program is free software; see the License file for more details.
+ * 
+ * You should have received a copy of the License along with this program.
+ * 
+ */
+
+
+#ifndef SESSION_H
+#define SESSION_H
+
+class Session
+{
+	public:
+		Session();
+		virtual ~Session();
+	
+	private:
+		/* add your private declarations */
+};
+
+#endif /* SESSION_H */ 
