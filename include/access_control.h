e5cd240fcbb5dd73e5b237b4a08823e49bf9afe2
Jose Felix Rivas Carrasco <josefelixrc@outlook.com>
2021-03-25 12:51:38 +0000

    feat: Add a class for Access control

---
+++ include/access_control.h
@@ -0,0 +1,26 @@
+/*
+ * access_control.h
+ * 
+ * Copyright 2021 CPW Online <support@cpwonline.org>
+ * 
+ * This program is free software; see the License file for more details.
+ * 
+ * You should have received a copy of the License along with this program.
+ * 
+ */
+
+
+#ifndef ACCESS_CONTROL_H
+#define ACCESS_CONTROL_H
+
+class AccessControl
+{
+	public:
+		AccessControl();
+		virtual ~AccessControl();
+	
+	private:
+		/* add your private declarations */
+};
+
+#endif /* ACCESS_CONTROL_H */ 
