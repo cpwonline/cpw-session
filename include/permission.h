d517101defd67d9ba174f2989b83fa4f3f4119a9
Jose Felix Rivas Carrasco <josefelixrc@outlook.com>
2021-03-25 12:46:54 +0000

    feat: Add a class for permissions

---
+++ include/permission.h
@@ -0,0 +1,26 @@
+/*
+ * permission.h
+ * 
+ * Copyright 2021 CPW Online <support@cpwonline.org>
+ * 
+ * This program is free software; see the License file for more details.
+ * 
+ * You should have received a copy of the License along with this program.
+ * 
+ */
+
+
+#ifndef PERMISSION_H
+#define PERMISSION_H
+
+class Permission
+{
+	public:
+		Permission();
+		virtual ~Permission();
+	
+	private:
+		/* add your private declarations */
+};
+
+#endif /* PERMISSION_H */ 
