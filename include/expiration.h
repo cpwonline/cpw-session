46b52914725ee9182829ffc7c80559fd6c18c560
Jose Felix Rivas Carrasco <josefelixrc@outlook.com>
2021-03-25 12:47:42 +0000

    feat: Add a class for expirations

---
+++ include/expiration.h
@@ -0,0 +1,26 @@
+/*
+ * expiration.h
+ * 
+ * Copyright 2021 CPW Online <support@cpwonline.org>
+ * 
+ * This program is free software; see the License file for more details.
+ * 
+ * You should have received a copy of the License along with this program.
+ * 
+ */
+
+
+#ifndef EXPIRATION_H
+#define EXPIRATION_H
+
+class Expiration
+{
+	public:
+		Expiration();
+		virtual ~Expiration();
+	
+	private:
+		/* add your private declarations */
+};
+
+#endif /* EXPIRATION_H */ 
