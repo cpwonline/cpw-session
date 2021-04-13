8dace726690dd1fe334735440d455dc7c93b50fa
Jose Felix Rivas Carrasco <josefelixrc@outlook.com>
2021-04-13 18:58:07 -0400

    test: Add `PermissionListTest` to `TestGen` class

--- tests/test_gen.cc
+++ tests/test_gen.cc
@@ -39,6 +39,29 @@ TEST_F(TestGen, UserNameTest)
 	;
 }
 
+TEST_F(TestGen, PermissionsListTest)
+{
+	TestObj_->get_current_session()->get_current_user()->set_user_name("josefelixrc7");
+	TestObj_->NewPermission_(true, "Menus", "Permissions for menus", true, true, true, false);
+	TestObj_->NewPermission_(true, "SubMenus", "Permissions for submenus", true, true, false, false);
+	TestObj_->NewPermission_(true, "Accounts", "Permissions for accounts", false, true, false, false);
+	TestObj_->NewPermission_(true, "Deals", "Permissions for deals", false, true, true, false);
+	
+	auto permissions = TestObj_->get_current_session()->get_permissions_colector();
+	std::cout << "\nSize: " << permissions->size();
+	for(auto it = permissions->begin(); it != permissions->end(); ++it)
+	{
+		std::cout << "\nname: " << (*it)->get_name() << ", passed: " << (*it)->get_passed() << 
+			", description: " << (*it)->get_description() <<
+			", DACList: "
+				<< (*it)->get_actions_list()->get_create()
+				<< (*it)->get_actions_list()->get_read()
+				<< (*it)->get_actions_list()->get_update()
+				<< (*it)->get_actions_list()->get_remove()
+		;
+	}
+	std::cout << "\n";
+}
 
 //-----------------------------------------------------------------------------
 
